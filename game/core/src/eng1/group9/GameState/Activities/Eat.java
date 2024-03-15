9db5fd22ecd2b284e05b3b68cb4d4ee967fe1367
James Lawson <67314546+JamesLawson01@users.noreply.github.com>
2024-03-15 13:57:45 +0000

    Fix activities

--- game/core/src/eng1/group9/GameState/Activities/Eat.java
+++ game/core/src/eng1/group9/GameState/Activities/Eat.java
@@ -1,11 +1,14 @@
-package eng1.group9.Activities;
+package eng1.group9.gamestate.Activities;
+
 
 /**
  * The class for the activity eat
  *
  */
+class Eat extends Activity {
 
-class Eat extends Activity{
 
-
+    public Eat(int time, int energy) {
+        super(time, energy);
+    }
 }
