97f245dc0e14240cc627614ae996e17b1f35302f
ArchieAdams <archieadams15@gmail.com>
2024-04-19 16:40:28 +0100

    Refactoring and testing setup

---
+++ tests/src/eng1/group9/tests/TestJunit.java
@@ -0,0 +1,25 @@
+package eng1.group9.tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+@RunWith(GdxTestRunner.class)
+public class TestJunit {
+
+    String message = "Hello World";
+
+    @Test
+    public void trueTest() {
+        assertEquals(message,"Hello World");
+    }
+
+    @Test
+    public void falseTest() {
+        assertNotEquals(message,"Hello");
+    }
+
+
+}
