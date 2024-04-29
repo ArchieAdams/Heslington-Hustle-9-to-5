81152c358f966b3bb2628db95f23dfd5049d154e
mitch <mitch.james.gilbert@gmail.com>
2024-04-29 19:09:45 +0100

    Refactor Activity class to Gym. Add FeedDucks, LakeRelax, Town classes to fulfil recreation requirement

--- core/src/eng1/group9/gamestate/activities/Gym.java
+++ core/src/eng1/group9/gamestate/activities/Gym.java
@@ -3,14 +3,14 @@ package eng1.group9.gamestate.activities;
 /**
  * A recreation activity
  */
-public class Recreation extends Activity {
+public class Gym extends Activity {
 
     /**
      * @param time   How much time the activity takes
      * @param energy How much energy the activity takes
      * @param name   Name of the activity
      */
-    public Recreation(int time, int energy, String name) {
+    public Gym(int time, int energy, String name) {
         super(time, energy, name);
     }
 
