7624613a7c75673590f0f12262cb4a4e9e2b87b2
mitch <mitch.james.gilbert@gmail.com>
2024-05-07 17:32:56 +0100

    Add achievement labels to EndScreenUI. Refactor EndScreenUi to move items out of update() function where not needed.

--- core/src/io/eng1/group9/endscreen/EndScreenUi.java
+++ core/src/io/eng1/group9/endscreen/EndScreenUi.java
@@ -18,11 +18,14 @@ class EndScreenUi extends ScreenUi {
 
   private final HustleGame game;
   private final ScreenViewport viewport;
+  private final Stage stage;
   private final Label thanksLabel;
   private final Label scoreLabel;
-  private final Stage stage;
   private final Label nameLabel;
-  private String name;
+  private Label eatLabel;
+  private Label studyLabel;
+  private Label recreationLabel;
+  private final Skin skin;
 
   /**
    * Instantiates a new End screen ui.
@@ -34,44 +37,87 @@ class EndScreenUi extends ScreenUi {
     viewport = new ScreenViewport();
     viewport.apply();
     stage = new Stage(viewport);
-    Skin skin = new Skin(Gdx.files.internal("Pixthulhu_UI_Skin/pixthulhuui/pixthulhu-ui.json"));
+    skin = new Skin(Gdx.files.internal("Pixthulhu_UI_Skin/pixthulhuui/pixthulhu-ui.json"));
 
-    thanksLabel = new Label("", skin);
-    thanksLabel.setAlignment(Align.center);
-
+    thanksLabel = new Label("Thank you for playing Heslington Hustle", skin);
     scoreLabel = new Label("", skin);
-    scoreLabel.setAlignment(Align.center);
+    nameLabel = new Label("Enter name: " + game.getGameState().getName(), skin);
 
-    nameLabel = new Label("", skin);
+    thanksLabel.setAlignment(Align.center);
+    scoreLabel.setAlignment(Align.center);
     nameLabel.setAlignment(Align.center);
+
+    eatLabel = null;
+    studyLabel = null;
+    recreationLabel = null;
 
     stage.addActor(thanksLabel);
     stage.addActor(scoreLabel);
     stage.addActor(nameLabel);
+
+    initializeScreenLayout();
+  }
+
+  private void initializeScreenLayout() {
+    Map<String, Integer> activitiesCount = game.getGameState().scoreCalculation();
+
+    int studyCount = activitiesCount.get("Study");
+    int eatCount = activitiesCount.get("Eat");
+    int recreationCount = activitiesCount.get("Recreation");
+
+    scoreLabel.setText(
+        String.format("On average per day you:\nStudied: %1d\nAte: %2d\nRelaxed: %3d\nScore: %4d",
+            Math.round(studyCount / 7f), Math.round(eatCount / 7f),
+            Math.round(recreationCount / 7f), game.getGameState().calculateScore()));
+
+    positionTopLabels();
+    positionBottomLabels(activitiesCount, studyCount, eatCount, recreationCount);
+  }
+
+  private void positionTopLabels() {
+    int width = viewport.getScreenWidth();
+    float height = viewport.getScreenHeight();
+
+    thanksLabel.setPosition((width - thanksLabel.getWidth()) / 2, height / 2 + 250);
+    scoreLabel.setPosition((width - scoreLabel.getWidth()) / 2, height / 2 + 110);
+  }
+
+  private void positionBottomLabels(Map<String, Integer> activitiesCount,
+                                    int studyCount,
+                                    int eatCount,
+                                    int recreationCount) {
+    int width = viewport.getScreenWidth();
+    int labelSpacing = 30;
+    int startY = viewport.getScreenHeight() / 2 - 50;
+
+    if (activitiesCount.containsKey("Eat") && eatCount > 1) {
+      eatLabel = new Label("Achievement: Eating", skin);
+      eatLabel.setPosition((width - eatLabel.getWidth()) / 2, startY);
+      stage.addActor(eatLabel);
+      startY -= labelSpacing;
+    }
+
+    if (activitiesCount.containsKey("Study") && studyCount > 1) {
+      System.out.println("studied");
+      studyLabel = new Label("Achievement: Study", skin);
+      studyLabel.setPosition((width - studyLabel.getWidth()) / 2, startY);
+      stage.addActor(studyLabel);
+      startY -= labelSpacing;
+    }
+
+    if (activitiesCount.containsKey("Recreation") && recreationCount > 1) {
+      recreationLabel = new Label("Achievement: Recreation", skin);
+      recreationLabel.setPosition((width - recreationLabel.getWidth()) / 2, startY);
+      stage.addActor(recreationLabel);
+      startY -= labelSpacing;
+    }
+    nameLabel.setPosition((width - nameLabel.getWidth()) / 2, (startY - labelSpacing));
   }
 
   @Override
   public void update() {
     ScreenUtils.clear(0, 0, 0, 255);
-    Map<String, Integer> activitiesCount = game.getGameState().scoreCalculation();
-    int studyCount = activitiesCount.get("Study");
-    int eatCount = activitiesCount.get("Eat");
-    int recreationCount = activitiesCount.get("Recreation");
-    thanksLabel.setText("Thank you for playing Heslington Hustle");
-    scoreLabel.setText(
-        String.format("On average per day you:\nStudied: %1d\nAte: %2d\nRelaxed: %3d\nScore: %4d",
-            Math.round(studyCount / 7f), Math.round(eatCount / 7f),
-            Math.round(recreationCount / 7f), game.getGameState().calculateScore()));
-    nameLabel.setText("Enter name: " +  game.getGameState().getName());
-
-    int width = viewport.getScreenWidth();
-    int height = viewport.getScreenHeight();
-    thanksLabel.setX((width - thanksLabel.getWidth()) / 2);
-    thanksLabel.setY((height - thanksLabel.getHeight()) / 2 + 150);
-    scoreLabel.setX((width - scoreLabel.getWidth()) / 2);
-    scoreLabel.setY((height - scoreLabel.getHeight()) / 2 - thanksLabel.getHeight() - 50);
-    nameLabel.setX((width - nameLabel.getWidth()) / 2);
-    nameLabel.setY((height - nameLabel.getHeight()) / 2 - nameLabel.getHeight() - 200);
+    nameLabel.setText("Enter name: " + game.getGameState().getName());
     stage.draw();
   }
 
